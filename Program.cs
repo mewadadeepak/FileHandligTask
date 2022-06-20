1ff07e35d0e7e4c363427cd26182fcbaabdd0b17
mewadadeepak <63440853+mewadadeepak@users.noreply.github.com>
2022-06-20 00:44:39 -0700

    Done
    Competed

--- Program.cs
+++ Program.cs
@@ -1,30 +1,39 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FileHandlingHandson
+using System.Data.SqlClient;
+using System.Xml;
+using System.IO;
+using System.Configuration;
+namespace Ado.net
+{ 
+public class Program 
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-
-            //1st task assign
-            //ReadFromTxtFile readFromTxtFile = new ReadFromTxtFile();
-            // readFromTxtFile.ReadFile(); 
-
-
-            //2nd taskassign
-            ReadMatch readMatching = new ReadMatch();
-
-            readMatching.Success();
-            //readMatch.Failed();
-            //readMatch.Dialled();
-            //readMatch.Missed();
-
-
-        }
+    public static void Main(string[] args){
+        string str = "Mumbai";
+        Program ob = new Program();
+        ob.DisplayCustomerByLocation(str);
     }
+    
+    public Dictionary<string,string> DisplayCustomerByLocation(string location){
+        Dictionary<string,string> d = new Dictionary<string,string>();
+        string xyz = ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString;
+        SqlConnection conn = new SqlConnection(xyz);
+        using(conn){
+            string q = "select AccountName, Location from Account where Location=@location order by AccountName asc";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(q,conn);
+            cmd.Parameters.AddWithValue("@location",location);
+            SqlDataReader rd = cmd.ExecuteReader();
+            while(rd.Read()){
+                string name = rd[0].ToString();
+                string loc = rd[1].ToString();
+                d.Add(name,loc);
+            }
+            conn.Close();
+        }
+        return d;
+    }
+  }
 }
+
---
+++ accountdetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Ado.net
+{
+    internal class accountdetails
+    {
+        public static string sqlConnectionStr = @"Data Source=DEEPAK;Initial Catalog=BankDb;Integrated Security=True";
+
+        public string InsertAccountHolder()
+        {
+
+            Console.WriteLine("Enter the Account Number");
+            int AccNum  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Account Holder  Name");
+            string AccHolName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Account  holder age");
+            int AccHolAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Account Holder  Address");
+            string AccHolAddress = Console.ReadLine();
+
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);
+            SqlCommand cmd = new SqlCommand("insert into AccountHolder values(" + AccNum + ",'" + AccHolName + "'," + AccHolAge + ",'" + AccHolAddress + "')", sqlConnection);
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            return "INSERTED";
+        }
+
+    }
+}
